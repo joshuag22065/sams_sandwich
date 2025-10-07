d5a9c15e464f9248cfc082491d0ea331e136ef28
joshuag22065 <joshuag.22065@obhs.school.nz>
2025-10-08 09:48:32 +1300

    Sam's sandwhich.v0.95_alpha

--- sams_sandwich.py
+++ sams_sandwich.py
@@ -1,3 +1,17 @@
+import datetime
+
+def force_number(message,lower,upper):
+    while True: #Infinite loop that loops until valid number is entered
+        try:
+            num=int(input(message))
+            if num>=lower and num<=upper:
+                break
+            else:
+                print(f"The number {num} is wrong, please type in a number between {lower}-{upper}")
+        except: #Only prints when users type in a string
+            print("Error, please enter in a number")
+    return num #Returns back valid number
+
 def bread_selection(): # Selects the bread
     bread_list = ["White", "Brown", "Italian", "Granary"]
     count = 0
@@ -5,7 +19,7 @@ def bread_selection(): # Selects the bread
     while count < len(bread_list): #prints out each item on the list
         print(count+1, " ", bread_list[count])
         count +=1
-    bread_selected = int(input("Which bread do you want? Enter a number: "))
+    bread_selected = force_number(("Which bread do you want? Enter a number: "), 1, len(bread_list))
     return bread_list[bread_selected-1] #Returns back a string
 
 def meat_selection():
@@ -15,7 +29,7 @@ def meat_selection():
     while count < len(meat_list): #prints out each item on the list
         print(count+1, " ", meat_list[count])
         count +=1
-    meat_selected = int(input("Which meat do you want? Enter a number: "))
+    meat_selected = force_number(("Which meat do you want? Enter a number: "), 1, len(meat_list))
     return meat_list[meat_selected-1] #Returns back a string
 
 def cheese_selection():
@@ -25,7 +39,7 @@ def cheese_selection():
     while count < len(cheese_list): #prints out each item on the list
         print(count+1, " ", cheese_list[count])
         count +=1
-    cheese_selected = int(input("Which cheese do you want? Enter a number: "))
+    cheese_selected = force_number(("Which cheese do you want? Enter a number: "), 1, len(cheese_list))
     return cheese_list[cheese_selected-1] #Returns back a string
 
 def salad_selection():
@@ -40,12 +54,33 @@ def salad_selection():
     selected_salad= " " # Promps the user to enter a number in to select a salad
 
     while selected_salad != "": # If enter is not pressed it will keep prompting you to select a salad
-        selected_salad =  input(f"What number salad do you want? Enter a number: ")
+        selected_salad =  force_number((f"What number salad do you want? Enter a number: "), 1, len(salad_list))
         if selected_salad != "" :# If you press enter this if statement wont run
             selected_salad= int(selected_salad)
-            #this variable keeps adding on each selected item from the salad list
-            salads_added = salads_added + " " + salad_list[selected_salad]
-        return salads_added.strip() # Removes empty space at the start of the string
+            salads_added = salads_added + " " + salad_list[selected_salad-1]
+    return salads_added.strip() # Removes empty space at the start of the string
+
+def sauce_selection():
+    sauce_list = [" Regular Mayonnaise", " Oil"," Red Wine Vinegar", " Yellow Mustard", " Honey Mustard", " Cheddar sauce", " BBQ", " Siracha", " Mariana Sauce", "Sweet Onion Teriyaki", "Peppercorn Ranch"]
+    count = 0
+    print("We have the following sauce: ")
+    while count < len(sauce_list): #prints out each item on the list
+        print(count+1, "", sauce_list[count])
+        count +=1
+    sauce_selected = force_number(("Which sauce do you want? Enter a number: "), 1, len(sauce_list))
+    return sauce_list[sauce_selected-1].strip() #Returns back a string
+
+def output_textfile(sandwich_order):
+    date_time = datetime.datetime.now()
+    outF=open("Sam's_sandwiches.txt", "a")
+    print("***The quote for the sandwich***")
+    outF.write(f"\nDate of booking: {date_time}")
+    for booking in sandwich_order:
+        print (booking)
+        outF.write(f"\n {booking}") #outputs each item to the text file
+    print("***End of quote***")
+    outF.close()
+
 #Main program
 print("Welcome to Sam's Sandwich Shop")
 bread_choice=bread_selection() # Creating a variable that calls up the bread function and  returns their choice
@@ -56,3 +91,13 @@ cheese_choice=cheese_selection() # Creating a variable that calls up the meat fu
 print(f"Your selected cheese: {cheese_choice}")
 salad_choice=salad_selection() # Creating a variable that calls up the meat function and  returns their choice
 print(f"Your selected salads: {salad_choice}")
+sauce_choice=sauce_selection() # Creating a variable that calls up the meat function and  returns their choice
+print(f"Your selected sauce: {sauce_choice}")
+#This is temporay code to test that the bread subroutine is working first_name=str(input("What is your first name?")) cell_phone=str(input("What is your cellphone?")) bread_choice=bread_selection() meat_choice=meat_selected)
+sandwich_order=[] #empty list #adding selected items to the list sandwhich_order.append(first_name) sandwhich_order.append(cell_phone)
+sandwich_order.append(f"Bread: {bread_choice}") 
+sandwich_order.append(f"Meat: {meat_choice}") 
+sandwich_order.append(f"Cheese: {cheese_choice}") 
+sandwich_order.append(f"Salad: {salad_choice}") 
+sandwich_order.append(f"Sauce: {sauce_choice}")
+output_textfile(sandwich_order)
